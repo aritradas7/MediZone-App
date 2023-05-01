4926321ea1419d72857d3bccefbcdb09bfa038ea
Reekparna Sen <reekparnasen@gmail.com>
2023-05-01 17:31:48 +0530

    modified:   frontSide/package-lock.json 	modified:   frontSide/src/app/Adminlogin/Adminlogin.component.ts 	modified:   frontSide/src/app/MRlogin/MRlogin.component.html

--- frontSide/src/app/Adminlogin/Adminlogin.component.ts
+++ frontSide/src/app/Adminlogin/Adminlogin.component.ts
@@ -14,14 +14,18 @@ export class AdminloginComponent implements OnInit {
     email1 = ''
     password1 = ''
 
-    constructor(private router: Router, location: LocationStrategy) { 
-        history.pushState(null, null, window.location.href);  
+    constructor(private router: Router, location: LocationStrategy) {
+        history.pushState(null, null, window.location.href);
         location.onPopState(() => {
         history.pushState(null, null, window.location.href);})
     }
 
     onlogin()
     {
+      if (localStorage['login_status'] != '0'){
+        alert("One user is already logged in. Please logout the user first.")
+      }
+      else {
         if(this.email1 == "reekparna" && this.password1 == "1234"){
             this.router.navigate(['/login/dashboard'])
         }
@@ -29,7 +33,7 @@ export class AdminloginComponent implements OnInit {
         {
             alert('enter the vaild password')
         }
-
+      }
     }
 
     ngOnInit() { }
